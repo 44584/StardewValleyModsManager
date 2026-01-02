dfea5a894762fafbcf7b6acae0a483d8be66db0f
gk <3515844584@qq.com>
2026-01-02 23:11:54 +0800

    简单的启动验证

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let smapi_path =
+        "C:/Program Files (x86)/Steam/steamapps/common/Stardew Valley/StardewModdingAPI.exe";
+    let mods_folder_path = "Mods_simple";
+
+    launch_stardew_valley(smapi_path, mods_folder_path);
+}
+
+fn launch_stardew_valley(smapi_path: &str, mods_folder_path: &str) {
+    let child = Command::new(smapi_path)
+        .arg("--mods-path")
+        .arg(mods_folder_path)
+        .spawn()
+        .unwrap();
+    eprintln!("{}已启动", child.id());
 }
